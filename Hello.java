04a6d07ce34ff1b64c8cf0cbc6d4191186bfc740
Indrit-Vaka <63372867+Indrit-Vaka@users.noreply.github.com>
2022-05-14 14:33:56 +0200

    Bank Console App Demo
    Bank Console App Demo By Indrit vaka

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.text.CollationElementIterator;
+
+/**
+ * By Indrit Vaka
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+
+        var account1 = new eBanking("Indrit");
+        var account2 =  new eBanking("vaka", "0037", 357890);
+    }
+
+}
