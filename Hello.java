385bd989622c4cda1e3cfde3455ec07bc31a1835
Indrit-Vaka <63372867+Indrit-Vaka@users.noreply.github.com>
2022-05-18 01:57:10 +0200

    Adding Write and Read Files
    new
    1. new class for easy creation and reading files
    2. All transactions now are stored on a Text file
    3. The app now can read from file and create those accounts

--- Hello.java
+++ Hello.java
@@ -1,4 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.text.CollationElementIterator;
+import java.util.Scanner;
 
 /**
  * By Indrit Vaka
@@ -7,8 +12,28 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        var account1 = new eBanking("Indrit");
-        var account2 =  new eBanking("vaka", "0037", 357890);
-    }
 
+        //lets get all the account form the FIle
+        //Secili entitet ndahet me ,
+
+        String[] data = FileIV.ReadAllLines("accounts.txt");
+
+        for (int i = 0; i < data.length; i++) {
+
+            String[] parts = data[i].split(",");
+            try {
+                new eBanking(parts[0], parts[1], Double.parseDouble(parts[2]));
+            } catch (Exception e) {
+                System.out.println("There was an error : " + e.getMessage());
+            }
+        }
+        eBanking.allAccountCreated = true; //kemi shtuar te gjith acc. I shfaqim perd te Menune
+
+        
+
+        var account1 = new eBanking("Indrit");
+        var account2 = new eBanking("vaka", "0037", 357890);
+
+        
+    }
 }
